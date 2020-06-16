dd1fbc5068d3fed4d20d75a781741cd07ed320e3
Rouvas <mail@gaiduchik.com>
2020-06-17 01:24:52 +0300

    Add new pages

--- bazarpnz/src/app/app.module.ts
+++ bazarpnz/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllAdvertsComponent } from './all-adverts/all-adverts.component';
+import { AdvertComponent } from './advert/advert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllAdvertsComponent,
+    AdvertComponent
   ],
   imports: [
     BrowserModule,
